f04020a96a6fcfe4a7b14441d2ad4d0ea89ac79c
studioplace <webstudioplace.com>
2017-03-23 12:59:13 -0400

    express server created

--- app/app.js
+++ app/app.js
@@ -1,11 +1,31 @@
-var http = require('http');
+var express = require('express');
+var app = express();
 
-var myServer = http.createServer(function (request, response)
- {
-  response.writeHead(200, {"Content-Type" : "text/html"});
-  response.write('<h1>Web Studio</h1>');
-  response.end();
+app.get('/', function(req, res) {
+  res.send('<h1>Web Studio Place</h1>');
 });
 
-myServer.listen(3000);
-console.log('listening on port:3000');
+var server = app.listen(4000, function() {
+  console.log('Listening on port 4000');
+});
+
+
+
+
+
+
+
+
+// var http = require('http');
+//
+// var myServer = http.createServer(function (request, response)
+//  {
+//   response.writeHead(200, {"Content-Type" : "text/html"});
+//   response.write('<h1>Web Studio</h1>');
+//   response.end();
+// });
+//
+// myServer.listen(3000);
+// console.log('listening on port:3000');
+//
+//
